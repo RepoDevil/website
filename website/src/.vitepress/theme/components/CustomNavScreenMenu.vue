6c309880f07d0b2e47ef6cec37fb2361a19c2141
Himekami <enderinexiledc@gmail.com>
2024-05-30 01:34:26 +0000

    Update CustomNavScreenMenu.vue

--- website/src/.vitepress/theme/components/CustomNavScreenMenu.vue
+++ website/src/.vitepress/theme/components/CustomNavScreenMenu.vue
@@ -28,7 +28,7 @@ const nav = computed(() => {
     if (!item.text.includes('{app_version}'))
       return item
 
-    const appVersion = release.stable.tag_name.substring(1)
+    const appVersion = release.stable.tag_name
 
     return {
       ...item,
